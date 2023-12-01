6deec8c7f88f909e584b822d1a776443cb5a0b8d
Aaron Espinosa <125299695+Aaronespi@users.noreply.github.com>
2023-12-01 09:01:32 +0100

    Fixing an error
    Fixed an error with the get.

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const { readFile } = require('fs').promises;
 const app = express();
 
-app.get('/', async (request, repsonse) => {
-    Response.send( await readFile('./index.html', 'utf8'));
+app.get('/', async (request, response) => {
+    response.send( await readFile('./index.html', 'utf8'));
 });
 
 app.listen(8080, () => {
